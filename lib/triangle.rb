7ca56f06fe4e53b162f8bb92c32bbb205dae6e19
Spencer Johnson <sljohnson2222@gmail.com>
2022-03-16 13:13:48 -0500

    commit tree c1eeadb40a9d855fe60eb60827b67e9e59253284

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
+require "pry"
+
 class Triangle
   # write code here
+
+
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+
+  def kind
+    if l1 == l2 == l3
+      puts "equilateral"
+    end
+
+  end
+
+
 end
+binding.pry
